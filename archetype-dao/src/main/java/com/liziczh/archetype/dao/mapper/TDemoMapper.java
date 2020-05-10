b241625a83b90f22b1e463238c5a0e419ce45bb0
liziczh <liziczh@foxmail.com>
2020-05-11 00:06:13 +0800

    mybatis demo

---
+++ archetype-dao/src/main/java/com/liziczh/archetype/dao/mapper/TDemoMapper.java
@@ -0,0 +1,10 @@
+package com.liziczh.archetype.dao.mapper;
+
+import org.springframework.stereotype.Repository;
+
+import com.liziczh.archetype.api.entity.TDemo;
+import com.liziczh.base.mybatis.repository.BaseRepository;
+
+@Repository
+public interface TDemoMapper extends BaseRepository<TDemo, Integer> {
+}
--- archetype-dao/src/test/java/com/liziczh/archetype/dao/DaoApplicationTest.java
+++ archetype-dao/src/test/java/com/liziczh/archetype/dao/DaoApplicationTest.java
@@ -3,17 +3,20 @@ package com.liziczh.archetype.dao;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.liziczh.archetype.dao.mapper.TDemoMapper;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class DaoApplicationTests {
+@ActiveProfiles("test")
+class DaoApplicationTest {
 	@Autowired
 	private TDemoMapper demoMapper;
 	@Test
 	void DemoTest() {
-		demoMapper.getDemoById(1);
+		demoMapper.get(1);
 	}
 }
