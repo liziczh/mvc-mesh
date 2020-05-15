97559ef78fc2906b149c8590f0b065226d2afbae
liziczh <liziczh@foxmail.com>
2020-05-15 17:09:15 +0800

    dao cache

--- archetype-dao/src/main/java/com/liziczh/archetype/dao/mapper/TDemoMapper.java
+++ archetype-dao/src/main/java/com/liziczh/archetype/dao/mapper/TDemoMapper.java
@@ -3,7 +3,7 @@ package com.liziczh.archetype.dao.mapper;
 import org.springframework.stereotype.Repository;
 
 import com.liziczh.archetype.api.entity.TDemo;
-import com.liziczh.base.mybatis.repository.BaseRepository;
+import com.liziczh.base.common.repository.BaseRepository;
 
 @Repository
 public interface TDemoMapper extends BaseRepository<TDemo, Integer> {
