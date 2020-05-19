ff88939a524bf54fd967becb6be36b87c3112221
liziczh <liziczh@foxmail.com>
2020-05-19 18:53:33 +0800

    ExceptionMail

--- archetype-mgm/src/main/java/com/liziczh/archetype/mgm/aop/MgmExceptionMailAop.java
+++ archetype-mgm/src/main/java/com/liziczh/archetype/mgm/aop/MgmExceptionMailAop.java
@@ -1,18 +1,15 @@
-package com.liziczh.archetype.service.aop;
+package com.liziczh.archetype.mgm.aop;
 
 import java.util.Date;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
-import com.liziczh.base.common.aop.BaseServiceAop;
+import com.liziczh.base.common.aop.BaseExceptionMailAop;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @Component
 @Aspect
-public class ServiceAop extends BaseServiceAop {
+public class MgmExceptionMailAop extends BaseExceptionMailAop {
 	@Override
 	protected void sendErrorMessage(Exception exception, String className, String methodName, String methodParams, Date occurTime) {
 	}
