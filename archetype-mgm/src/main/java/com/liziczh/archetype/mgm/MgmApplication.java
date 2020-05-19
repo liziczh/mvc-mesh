3611f5c54883fc6b557e286f3acc8f7059380037
liziczh <liziczh@foxmail.com>
2020-05-19 14:36:12 +0800

    web config

--- archetype-mgm/src/main/java/com/liziczh/archetype/mgm/MgmApplication.java
+++ archetype-mgm/src/main/java/com/liziczh/archetype/mgm/MgmApplication.java
@@ -3,9 +3,13 @@ package com.liziczh.archetype.mgm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.liziczh.archetype.dao.DaoApplication;
+import com.liziczh.archetype.ref.RefApplication;
+
 @SpringBootApplication
 public class MgmApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(MgmApplication.class, args);
+		Class<?>[] sources = new Class<?>[] { DaoApplication.class, MgmApplication.class, RefApplication.class };
+		SpringApplication.run(sources, args);
 	}
 }
--- archetype-web/src/main/java/com/liziczh/archetype/web/WebApplication.java
+++ archetype-web/src/main/java/com/liziczh/archetype/web/WebApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.liziczh.archetype.dao.DaoApplication;
+import com.liziczh.archetype.ref.RefApplication;
 import com.liziczh.archetype.service.ServiceApplication;
 
 @SpringBootApplication(scanBasePackages = { "com.liziczh.*" })
 public class WebApplication {
 	public static void main(String[] args) {
-		Class<?>[] sources = new Class<?>[] { DaoApplication.class, ServiceApplication.class, WebApplication.class};
+		Class<?>[] sources = new Class<?>[] { DaoApplication.class, ServiceApplication.class, RefApplication.class, WebApplication.class };
 		SpringApplication.run(sources, args);
 	}
 }
