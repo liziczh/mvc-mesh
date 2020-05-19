9db51ff74b486c122c6fd2aca61b5920f758c580
liziczh <liziczh@foxmail.com>
2020-05-19 16:11:53 +0800

    slf4j

--- archetype-service/src/main/java/com/liziczh/archetype/service/aop/ServiceAop.java
+++ archetype-service/src/main/java/com/liziczh/archetype/service/aop/ServiceAop.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Component;
 
 import com.liziczh.base.service.aop.BaseServiceAop;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 @Aspect
 public class ServiceAop extends BaseServiceAop {
