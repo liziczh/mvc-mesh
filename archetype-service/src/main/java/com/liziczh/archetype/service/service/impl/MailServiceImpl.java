358f3ab1d0d6eb41609243a9c33653a11981aec5
liziczh <liziczh@foxmail.com>
2020-06-07 01:17:21 +0800

    Mail

---
+++ archetype-service/src/main/java/com/liziczh/archetype/service/service/impl/MailServiceImpl.java
@@ -0,0 +1,9 @@
+package com.liziczh.archetype.service.service.impl;
+
+import com.liziczh.archetype.api.service.MailService;
+
+public class MailServiceImpl implements MailService {
+	@Override
+	public void sendHTMLMail(String to, String subject, String content) {
+	}
+}
