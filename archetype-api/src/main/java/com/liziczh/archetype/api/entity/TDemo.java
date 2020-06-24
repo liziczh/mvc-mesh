48810a5e599d46d67973cb3978bad5598173ad5a
liziczh <liziczh@foxmail.com>
2020-06-24 16:56:01 +0800

    kafka配置

--- archetype-api/src/main/java/com/liziczh/archetype/api/entity/TDemo.java
+++ archetype-api/src/main/java/com/liziczh/archetype/api/entity/TDemo.java
@@ -4,18 +4,26 @@ import java.util.Date;
 
 import com.liziczh.base.common.entity.BaseEntity;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
 public class TDemo extends BaseEntity {
 	private static final long serialVersionUID = -3399850105386695874L;
+	@ApiModelProperty(value = "ID")
 	private Integer id;
+	@ApiModelProperty(value = "姓名")
 	private String name;
+	@ApiModelProperty(value = "创建时间")
 	private Date createTime;
+	@ApiModelProperty(value = "创建人")
 	private String createUser;
+	@ApiModelProperty(value = "更新时间")
 	private Date updateTime;
+	@ApiModelProperty(value = "更新人")
 	private String updateUser;
+	@ApiModelProperty(value = "权限角色")
 	private String permitRole;
+	@ApiModelProperty(value = "是否有效")
 	private String valid;
 }
--- archetype-service/src/main/java/com/liziczh/archetype/service/service/impl/MailServiceImpl.java
+++ archetype-service/src/main/java/com/liziczh/archetype/service/service/impl/MailServiceImpl.java
@@ -2,6 +2,10 @@ package com.liziczh.archetype.service.service.impl;
 
 import com.liziczh.archetype.api.service.MailService;
 
+/**
+ * 邮件服务
+ * @author zhehao.chen
+ */
 public class MailServiceImpl implements MailService {
 	@Override
 	public void sendHTMLMail(String to, String subject, String content) {
