e9d07f33686addc98b7ebba3808227958cf1ea26
liziczh <liziczh@foxmail.com>
2020-06-26 00:25:10 +0800

    Mail

--- archetype-service/src/main/java/com/liziczh/archetype/service/service/impl/MailServiceImpl.java
+++ archetype-service/src/main/java/com/liziczh/archetype/service/service/impl/MailServiceImpl.java
@@ -1,11 +1,14 @@
 package com.liziczh.archetype.service.service.impl;
 
+import org.springframework.stereotype.Component;
+
 import com.liziczh.archetype.api.service.MailService;
 
 /**
  * 邮件服务
  * @author zhehao.chen
  */
+@Component
 public class MailServiceImpl implements MailService {
 	@Override
 	public void sendHTMLMail(String to, String subject, String content) {
