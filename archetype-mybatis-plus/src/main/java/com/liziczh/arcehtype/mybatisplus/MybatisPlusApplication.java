b485ca88e0581785d130f07eb016f8d14787cb86
liziczh <liziczh@foxmail.com>
2020-07-21 17:05:52 +0800

    data-jpa  &  mybatis-plus

---
+++ archetype-mybatis-plus/src/main/java/com/liziczh/arcehtype/mybatisplus/MybatisPlusApplication.java
@@ -0,0 +1,10 @@
+package com.liziczh.arcehtype.mybatisplus;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusApplication {
+	public static void main(String[] args) {
+		MybatisPlusApplication.run(MybatisPlusApplication.class, args);
+	}
+}
---
+++ archetype-mybatis-plus/src/test/java/com/liziczh/arcehtype/mybatisplus/MybatisPlusApplicationTest.java
@@ -0,0 +1,11 @@
+package com.liziczh.arcehtype.mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusApplicationTest {
+	@Test
+	void contextLoads() {
+	}
+}
