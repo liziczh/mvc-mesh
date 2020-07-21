9f3f3d99b5bee36c0de1713f798171ae7ac2051d
liziczh <liziczh@foxmail.com>
2020-07-21 18:26:38 +0800

    DAO

---
+++ archetype-jpa/src/test/java/com/liziczh/arcehtype/data/jpa/SpringDataJpaApplicationTest.java
@@ -0,0 +1,18 @@
+package com.liziczh.arcehtype.data.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringJUnitConfig(classes = SpringDataJpaApplication.class)
+@SpringBootTest
+@ActiveProfiles("test")
+class SpringDataJpaTest {
+	@Test
+	void contextLoads() {
+	}
+}
---
+++ archetype-mybatis-plus/src/test/java/com/liziczh/archetype/mybatis/plus/MybatisPlusTest.java
@@ -0,0 +1,11 @@
+package com.liziczh.module.mybatis.plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusTest {
+	@Test
+	void contextLoads() {
+	}
+}
