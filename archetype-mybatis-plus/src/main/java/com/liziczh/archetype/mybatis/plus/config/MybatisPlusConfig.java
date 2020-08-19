54e718504744d89cc8b5c810063f3d31620d03d5
liziczh <liziczh@foxmail.com>
2020-08-19 09:34:52 +0800

    mybatisplus

--- archetype-mybatis-plus/src/main/java/com/liziczh/archetype/mybatis/plus/config/MybatisPlusConfig.java
+++ archetype-mybatis-plus/src/main/java/com/liziczh/archetype/mybatis/plus/config/MybatisPlusConfig.java
@@ -4,13 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 
 @MapperScan("com.liziczh.**.mapper")
 @EntityScan(basePackages = "com.liziczh.**.entity")
-@EnableTransactionManagement
 @Configuration
 public class MybatisPlusConfig {
 	/**
