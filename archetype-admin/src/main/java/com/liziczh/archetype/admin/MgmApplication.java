9a7aa56f4265906198a56244b05ef54ea63c26e4
liziczh <liziczh@foxmail.com>
2020-11-26 17:52:11 +0800

    Application

--- archetype-admin/src/main/java/com/liziczh/archetype/admin/MgmApplication.java
+++ archetype-admin/src/main/java/com/liziczh/archetype/admin/MgmApplication.java
@@ -3,13 +3,14 @@ package com.liziczh.archetype.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.liziczh.archetype.internal.InternalApplication;
 import com.liziczh.archetype.mybatisplus.MybatisPlusApplication;
 import com.liziczh.archetype.ref.RefApplication;
 
 @SpringBootApplication
 public class MgmApplication {
 	public static void main(String[] args) {
-		Class<?>[] sources = new Class<?>[] { MybatisPlusApplication.class, MgmApplication.class, RefApplication.class };
+		Class<?>[] sources = new Class<?>[] { MybatisPlusApplication.class, RefApplication.class, InternalApplication.class, MgmApplication.class };
 		SpringApplication.run(sources, args);
 	}
 }
--- archetype-web/src/main/java/com/liziczh/archetype/web/WebApplication.java
+++ archetype-web/src/main/java/com/liziczh/archetype/web/WebApplication.java
@@ -3,6 +3,7 @@ package com.liziczh.archetype.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.liziczh.archetype.internal.InternalApplication;
 import com.liziczh.archetype.mybatisplus.MybatisPlusApplication;
 import com.liziczh.archetype.ref.RefApplication;
 import com.liziczh.archetype.service.ServiceApplication;
@@ -10,7 +11,7 @@ import com.liziczh.archetype.service.ServiceApplication;
 @SpringBootApplication(scanBasePackages = { "com.liziczh.*" })
 public class WebApplication {
 	public static void main(String[] args) {
-		Class<?>[] sources = new Class<?>[] { MybatisPlusApplication.class, ServiceApplication.class, RefApplication.class, WebApplication.class };
+		Class<?>[] sources = new Class<?>[] { MybatisPlusApplication.class, RefApplication.class, InternalApplication.class, ServiceApplication.class, WebApplication.class };
 		SpringApplication.run(sources, args);
 	}
 }
